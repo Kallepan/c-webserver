ea7dfa80261114fa1f795f6fa431c81c4ee862c1
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-12-31 12:26:29 +0000

    feat: simple web server implementation

--- src/server.cc
+++ src/server.cc
@@ -12,14 +12,85 @@
 
 #define PORT 8080
 
+int setHeader(std::string &input, int status_code, std::string status_message, int content_length, std::string content_type)
+{
+    /**
+     * @brief Get the header from the request
+     * @param input The request
+     * @param status_code The status code
+     * @param status_message The status message
+     * @param content_length The content length
+     * @param content_type The content type
+     * @return error code
+     */
+
+    // clear header
+    input = "";
+
+    // set Default header
+    // consider std::stringstream if this gets too messy
+    input += "HTTP/1.1 " + std::to_string(status_code) + " " + status_message + "\n";
+    input += "Content-Type: " + content_type + "\n";
+    input += "Content-Length: " + std::to_string(content_length) + "\n\n"; // note the double line feed (RFC 2616 requires this)
+
+    return 0;
+}
+
+int addContent(std::string &input, std::string content)
+{
+    /**
+     * @brief Add content to the request
+     * @param input The request
+     * @param content The content to add
+     * @return error code
+     */
+
+    input += content;
+
+    return 0;
+}
+
+int parseBuffer(std::string &buffer, std::string &path)
+{
+    /**
+     * @brief Parse the buffer to extract the GET request, we ignore the rest
+     * @return error code
+     */
+
+    // find GET request
+    std::size_t found = buffer.find("GET");
+    if (found == std::string::npos)
+    {
+        std::cout << "No GET request found\n";
+        return 1;
+    }
+
+    // find end of GET request
+    found = buffer.find("HTTP/1.1");
+    if (found == std::string::npos)
+    {
+        std::cout << "No end of GET request found\n";
+        return 1;
+    }
+
+    // extract GET request
+    path = buffer.substr(4, found - 5);
+
+    // if path is empty, set to index.html
+    if (path == "" || path == "/")
+    {
+        path = "/index.html";
+    }
+
+    return 0;
+}
+
 int main()
 {
     int server_fd, new_socket;
     long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-
-    std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) // AF_INET: IPv4, SOCK_STREAM: TCP, 0: IP
@@ -33,10 +104,10 @@ int main()
 
     std::fill(std::begin(address.sin_zero), std::end(address.sin_zero), '\0'); // Fill with 0
 
-    // Forcefully attaching socket to the port 8080
+    // Forcefully attaching socket to the defined port
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) // Bind socket to address
     {
-        throw std::runtime_error("bind failed");
+        throw std::runtime_error("port bind failed");
     }
     if (listen(server_fd, 10) < 0) // Listen for connections
     {
@@ -53,9 +124,54 @@ int main()
         std::string buffer(30000, '\0');                       // Buffer for message
         valread = read(new_socket, &buffer[0], buffer.size()); // Read message into buffer
         buffer.resize(valread);                                // Resize buffer to message size
-        std::cout << buffer << '\n';                           // Print message
-        write(new_socket, hello.c_str(), hello.length());      // Send message
-        std::cout << "------------------Hello message sent-------------------\n";
+        std::cout << buffer << '\n';                           // Print buffer
+
+        // Parse buffer and get GET request
+        std::string path;
+        parseBuffer(buffer, path);
+
+        // using the path, get the content
+        std::ifstream file("../res" + path);
+        if (!file)
+        {
+
+            // file not found
+            std::string message, content = "File not found: " + path + "\n";
+
+            // quick logs :
+            std::cout << content;
+
+            setHeader(message, 404, "Not Found", content.length(), "text/plain");
+            addContent(message, content);
+
+            // send header and content
+            write(new_socket, message.c_str(), message.length());
+            close(new_socket);
+
+            continue;
+        }
+
+        // get file size
+        file.seekg(0, std::ios::end);
+        std::streampos length = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        // read file into buffer
+        std::vector<char> buffer_file(length);
+        file.read(&buffer_file[0], length);
+
+        // convert buffer to string
+        std::string content(buffer_file.begin(), buffer_file.end());
+
+        // set header
+        std::string header;
+        setHeader(header, 200, "OK", content.length(), "text/html");
+        addContent(header, content);
+
+        // send header and content
+        write(new_socket, header.c_str(), header.length());
+
+        std::cout << "------------------ Message sent -------------------\n";
         close(new_socket); // Close connection
     }
 
